b388d5f39438268779ddeb8d2ed7f5fd61ac0b64
wdstar <wdstar@users.noreply.github.com>
2021-10-10 10:35:25 +0900

    refactoring.

--- bin/install-tools.sh
+++ bin/install-tools.sh
@@ -2,5 +2,9 @@
 
 echo 'Installing tools...'
 
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
+BASE_DIR=`dirname $0`
+SCRIPT_DIR=`cd $BASE_DIR; pwd`
+# on Bash
+#SCRIPT_DIR=$(cd $(dirname $0); pwd)
 echo "SCRIPT_DIR: ${SCRIPT_DIR}"
+echo "PWD: ${PWD}"
